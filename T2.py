65e3175fbb4cbad75114986d1799f70136345468
charishma2122 <54016737+charishma2122@users.noreply.github.com>
2023-11-21 19:08:52 +0530

    Update T2.py

--- T2.py
+++ T2.py
@@ -2,5 +2,5 @@ def read_file(file_name):
         with open(file_name, 'r') as file: # open the text file to read the content
             content_from_file = file.read()  # content in the file was assigned to variable
             print("Conent from the text",content_from_file)
-file_name = 'Charishma file2023-11-20 22_01_13.199131.txt'  # you can change the your file
+file_name = 'Charishma_file2023-11-21 18_50_58.084244.txt'  # you can change the your file 
 read_file(file_name) # call the function
